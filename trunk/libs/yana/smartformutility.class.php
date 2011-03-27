10b313436a7d1a10401d95ea8bdec660beaa19d6
yanaFramework_cp <SND\yanaFramework_cp>
2011-03-27 21:50:28 +0000

    splitting some parts off the too big SmartUtility class

---
+++ trunk/libs/yana/smartformutility.class.php
@@ -0,0 +1,294 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ *
+ * @ignore
+ */
+
+/**
+ * <<utility>> SmartUtility
+ *
+ * This is a utility class. It encapsulates extensions to use with
+ * the smarty temlate engine.
+ *
+ * This is also a global namespace for layout specific functions.
+ * These functions implement recursive replacement of tokens.
+ * This functionality is used for registry-files (sml/config)
+ * of the framework.
+ *
+ * {@internal
+ *
+ * Additional smarty functions and modifiers are documented elsewhere.
+ * These functions should be ignored in API-documentation.
+ *
+ * }}
+ *
+ * @static
+ * @access      public
+ * @package     yana
+ * @subpackage  core
+ * @ignore
+ */
+class SmartFormUtility extends Utility
+{
+
+    /**
+     * <<smarty function>> create
+     *
+     * This is a generator function to create dynamic HTML forms from database
+     * schema files and database values.
+     * Forms with several layouts can be generated to view, edit, insert, delete
+     * or search database values.
+     *
+     * If necessary it communicates with the database to retrieve values.
+     * This communication is limited to Select-Statements. No data is changed.
+     *
+     * For security reasons this function does NOT provide functionality to
+     * write changes made in the forms to the database on it's own.
+     * This might have introduced the possibility to compromise the security settings
+     * of third-party plugIns and thus has never been implemented and never will be.
+     *
+     * Instead you need to provide the name of a function in your plugin,
+     * that will handle the requests. This gives you the chance to do additional
+     * security checks and filter the provided data as you see fit.
+     *
+     * <pre>
+     * This function takes the following arguments:
+     *
+     * string  $file        (mandatory) path and name of structure file
+     * string  $table       (optional)  table to choose from structure file
+     * string  $id          (optional)  name of form to use (either $id or $table must be present!)
+     * string  $show        (optional)  comma seperated list of columns,
+     *                                  that should be shown in the form
+     * string  $hide        (optional)  comma seperated list of columns,
+     *                                  that should NOT be shown in the form
+     * string  $where       (optional)  sequence for SQL-where clause
+     *                                  <FIELDNAME1>=<VALUE1>[,<FIELDNAME2>=<VALUE2>[,...]]
+     * string  $sort        (optional)  name of column to sort entries by
+     * boolean $desc        (optional)  sort entries in descending (true) or ascending (false) order
+     * integer $page        (optional)  number of 1st entry to show
+     * integer $entries     (optional)  number of entries to show on each page
+     * string  $oninsert    (optional)  name of action (plugin-function) to execute on the event
+     * string  $onupdate    (optional)  name of action (plugin-function) to execute on the event
+     * string  $ondelete    (optional)  name of action (plugin-function) to execute on the event
+     * string  $onsearch    (optional)  name of action (plugin-function) to execute on the event
+     * string  $ondownload  (optional)  name of action (plugin-function) to execute on the event
+     * string  $onexport    (optional)  name of action (plugin-function) to execute on the event
+     * int     $layout      (optional)  where multiple layouts are available to present the result,
+     *                                  this allows to choose the prefered one
+     * </pre>
+     *
+     * Example of usage:
+     * <code>
+     * {create file="guestbook" table="guestbook" sort="guestbook_date" desc="true"}
+     * </code>
+     *
+     * @access  public
+     * @static
+     * @param   array   $params  see arguments list above
+     * @return  string
+     * @ignore
+     */
+    public static function createForm(array $params)
+    {
+        /**
+         * parameter 'file'
+         *
+         * This parameter is mandatory.
+         */
+        if (!isset($params['file']) || !is_string($params['file'])) {
+            return "";
+        }
+
+        // create database query
+        $database = Yana::connect($params['file']);
+        $dbSchema = $database->getSchema();
+
+        switch (true)
+        {
+            /**
+             * parameter 'id'
+             *
+             * This parameter is mandatory, if the parameter 'table' is not present.
+             */
+            case isset($params['id']):
+                if (!$dbSchema->isForm($params['id'])) {
+                    return "The form with name '" . $params['id'] . "' was not found.";
+                }
+                $form = $database->getForm($params['id']);
+            break;
+            /**
+             * parameter 'table'
+             *
+             * This parameter is mandatory. When present, the parameter 'id' is ignored.
+             */
+            case isset($params['table']):
+
+                $genericName = $dbSchema->getName() . '-' . $params['table'];
+
+                if ($dbSchema->isForm($genericName)) { // form already exists
+                    $form = $dbSchema->getForm($genericName);
+                    assert('$form instanceof DDLAbstractForm; // form not found');
+                    break;
+                }
+
+                $table = $dbSchema->getTable($params['table']);
+                if (! $table instanceof DDLTable) {
+                    return ""; // error - table not found
+                }
+
+                // create new form object
+                $form = $dbSchema->addForm($genericName, 'DDLDefaultForm');
+                unset($genericName);
+
+                /**
+                 * create query
+                 */
+                $query = new DbSelect($database);
+                $query->setTable($table->getName());
+
+                /**
+                 * parameters 'show', 'hide'
+                 *
+                 * These black- and whitelist params are used to create
+                 * the column list for the query.
+                 */
+                $showColumns = array();
+                if (isset($params['show'])) {
+                    if (!is_array($params['show'])) {
+                        $showColumns = explode(',', $params['show']);
+                    } else {
+                        $showColumns = $params['show'];
+                    }
+                }
+                $hideColumns = array();
+                if (isset($params['hide'])) {
+                    if (!is_array($params['hide'])) {
+                        $hideColumns = explode(',', $params['hide']);
+                    } else {
+                        $hideColumns = $params['hide'];
+                    }
+                }
+
+                /**
+                 * set columns
+                 *
+                 * This column list is used to auto-generate a list of DDLDefaultField entries
+                 * for the generated form.
+                 */
+                $columnNames = array_diff($showColumns, $hideColumns);
+                if (!empty($columnNames)) {
+                    $query->setColumns($columnNames);
+                } else {
+                    $columnNames = $table->getColumnNames();
+                }
+                unset($showColumns, $hideColumns);
+
+                $form->setQuery($query);
+            break; // end if params[table]
+            /**
+             * error - missing on of the followin params: 'id', 'table'
+             */
+            default:
+                return "";
+            break;
+        }
+
+        if (!$form->isCached()) {
+            $formSetup = new FormSetup();
+            /**
+             * parameters 'sort', 'desc'
+             *
+             * set 'orderBy' - clause
+             */
+            if (isset($params['sort'])) {
+                try {
+                    $form->getQuery()->setOrderBy($params['sort'], !empty($params['desc']));
+                } catch (NotFoundException $e) {
+                    return "";
+                }
+            }
+
+            /**
+             * parameter 'where'
+             */
+            if (!empty($params['where'])) {
+                try {
+                    $form->getQuery()->setWhere($params['where']);
+                } catch (Exception $e) {
+                    return "";
+                }
+            }
+
+            if (isset($params['on_insert'])) {
+                $form->setInsertAction($params['on_insert']);
+            }
+            if (isset($params['on_update'])) {
+                $form->setUpdateAction($params['on_update']);
+            }
+            if (isset($params['on_delete'])) {
+                $form->setDeleteAction($params['on_delete']);
+            }
+            if (isset($params['on_search'])) {
+                $form->setSearchAction($params['on_search']);
+            }
+            if (isset($params['on_export'])) {
+                $form->setExportAction($params['on_export']);
+            }
+            if (isset($params['on_download'])) {
+                $form->setDownloadAction($params['on_download']);
+            } else {
+                // initialize download action
+                $form->setDownloadAction('download_file');
+            }
+            if (isset($params['layout'])) {
+                $formSetup->setLayout((int) $params['layout']);
+                $form->setLayout((int) $params['layout']); // @deprecated
+            }
+
+            /**
+             * parameter 'page'
+             */
+            if (isset($params['page'])) {
+                $formSetup->setPage((int) $params['page']);
+                $form->setPage((int) $params['page']); // @deprecated
+            }
+            /**
+             * parameter 'entries'
+             */
+            if (isset($params['entries'])) {
+                $formSetup->setEntriesPerPage((int) $params['entries']);
+                $form->setEntriesPerPage((int) $params['entries']); // @deprecated
+            }
+            $form->__wakeup(); // @deprecated
+        }
+        $formSetup->updateSetup(Request::getVars());
+
+        return $form->toString(); // @deprecated
+    }
+
+}
+
+?>
