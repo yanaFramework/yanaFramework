e6952e4551f5a0b0e775151213c713e68dd369b9
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2011-04-14 17:28:28 +0000

    new scenario class, holding the context of a form

---
+++ trunk/libs/yana/formsetupcontext.class.php
@@ -0,0 +1,176 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+/**
+ * Form settings that depend on the type of form used.
+ *
+ * A context is a number of settings that apply to a form in a specified scenario.
+ * E.g. using an insert-context a form may have other contents than in an edit- or search-context.
+ *
+ * @access      public
+ * @package     yana
+ * @subpackage  form
+ */
+class FormSetupContext extends Object
+{
+
+    /**
+     * Form action name.
+     *
+     * @access  private
+     * @var     string
+     * @ignore
+     */
+    private $_action = "";
+
+    /**
+     * Values.
+     *
+     * @access  private
+     * @var     string
+     * @ignore
+     */
+    private $_values = array();
+
+    /**
+     * Optional list of included column names.
+     *
+     * Empty = all.
+     *
+     * @access  private
+     * @var     array
+     */
+    private $_columnNames = array();
+
+    /**
+     * get form value
+     *
+     * @access  public
+     * @param   string  $key  id of value to retrieve
+     * @return  mixed
+     */
+    public function getValue($key)
+    {
+        assert('is_string($key); // Wrong argument type argument 1. String expected');
+        return Hashtable::get($this->_values, strtolower($key));
+    }
+
+    /**
+     * Get form values.
+     *
+     * @access  public
+     * @return  array
+     */
+    public function getValues()
+    {
+        assert('is_array($this->_values); // Member "values" is expected to be an array.');
+        return $this->_values;
+    }
+
+    /**
+     * Set form value.
+     *
+     * @access  public
+     * @param   string  $key    id of value to set
+     * @param   mixed   $value  new value
+     * @return  FormSetup
+     */
+    public function setValue($key, $value)
+    {
+        assert('is_string($key); // Wrong argument type argument 1. String expected');
+        $this->_values[$key] = $value;
+        return $this;
+    }
+
+    /**
+     * Set form values.
+     *
+     * @access  public
+     * @param   array  $values  new values
+     * @return  FormSetup
+     */
+    public function setValues(array $values)
+    {
+        $this->_values = $values;
+        return $this;
+    }
+
+    /**
+     * set export action
+     *
+     * @access  public
+     * @param   string  $action action name
+     * @return  FormSetup
+     */
+    public function setAction($action)
+    {
+        assert('is_string($action); // Wrong type for argument 1. String expected');
+        $this->_action = $action;
+        return $this;
+    }
+
+    /**
+     * get export action
+     *
+     * @access  public
+     * @return  string
+     */
+    public function getAction()
+    {
+        return $this->_action;
+    }
+
+    /**
+     * Get unique list of column names.
+     *
+     * @access  public
+     * @return  array
+     */
+    public function getColumnNames()
+    {
+        return $this->_columnNames;
+    }
+
+    /**
+     * Set unique list of column names.
+     *
+     * This does not check if the columns do exist.
+     * If the list is left empty. The form is meant to auto-detect the abvailable columns.
+     *
+     * @access  public
+     * @param   array  $columnNames  list of identifiers
+     * @return  FormSetup
+     */
+    public function setColumnNames(array $columnNames)
+    {
+        $this->_columnNames = $columnNames;
+        return $this;
+    }
+
+}
+
+?>
