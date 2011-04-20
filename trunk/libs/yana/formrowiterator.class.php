ec0c1a808c50bda5ef81ef47170f7dde7e1abbeb
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2011-04-20 23:03:01 +0000

    new class

---
+++ trunk/libs/yana/formrowiterator.class.php
@@ -0,0 +1,60 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+/**
+ * <<iterator>> for table rows.
+ *
+ * An outer iterator, that allows to iterate over the rows of a table layout
+ *
+ * @access      public
+ * @package     yana
+ * @subpackage  form
+ */
+class FormRowIterator extends Collection
+{
+
+    /**
+     * Insert or replace a row.
+     *
+     * @access  public
+     * @param   string  $offset  index of item to replace
+     * @param   array   $row     new value of item
+     * @throws  InvalidArgumentException
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (is_array($value)) {
+            parent::offsetSet($offset, $value);
+        } else {
+            $message = "Array expected. Found " . gettype($value) . " instead.";
+            throw new InvalidArgumentException($message);
+        }
+    }
+
+}
+
+?>
