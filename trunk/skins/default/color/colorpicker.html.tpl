3ffd4bc632faa91343c5f6f33fc0a7a94d639da7
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2011-06-03 22:37:23 +0000

    changes to colorpicker

--- trunk/skins/default/color/colorpicker.html.tpl
+++ trunk/skins/default/color/colorpicker.html.tpl
@@ -4,15 +4,15 @@
 	Smarty template
 *}
 
-<a target="_blank" onclick="showColorPicker(event, '{$target}'); return false;" href={"action=colorpicker"|href}><img border="0" alt="" hspace="5" src="button.jpg"/></a>
-<script type="text/javascript" src="program.js"></script>
+<a target="_blank" onclick="showColorPicker(event, '{$target}'); return false;"
+    href={"action=colorpicker"|href}><img border="0" alt="" hspace="5" src="button.jpg"/></a>
+<script type="text/javascript" src="colorpicker.js"></script>
 <script type="text/javascript">// <![CDATA[
-{literal}
 if (!document.getElementById('_colorpicker')) {
     document.write('<div id="_colorpicker" style="display: none;" class="floating_menu"></div>');
     var colorPicker = new AjaxRequest(document.location.href);
     colorPicker.setTarget('_colorpicker');
     colorPicker.send('action=colorpicker');
 }
-{/literal}// ]]>
+// ]]>
 </script>
--- trunk/skins/default/color/colorpicker.js
+++ trunk/skins/default/color/colorpicker.js
@@ -8,113 +8,18 @@
 /* initialize */
 var over=false;
 var sat=0;
-var sat_alt=0;
+var sat_old=0;
 var hue=0;
-var hue_alt=0;
+var hue_old=0;
 var light=0;
-var light_alt=0;
+var light_old=0;
 var red=255;
 var blue=0;
 var green=0;
 var redHex='FF';
 var blueHex='00';
 var greenHex='00';
-var hexadez  = '#%REDHEX%%GREENHEX%%BLUEHEX%';
-
-function setHue(value)
-{
-    if (check('hue', value)) {
-        parent.hue=value;
-    }
-    calcColor();
-    showColor();
-}
-
-function setLight(value)
-{
-    if (check('light', value)) {
-        parent.light=value;
-    }
-    calcColor();
-    showColor();
-}
-
-function setSat(value)
-{
-    if (check('sat', value)) {
-        parent.sat=255-value;
-    }
-    calcColor();
-    showColor();
-}
-
-function setRed(value)
-{
-    if (check('red', value)) {
-        parent.red=value*1;
-    }
-    showColor();
-}
-
-function setGreen(value)
-{
-    if (check('green', value)) {
-        parent.green=value*1;
-    }
-    showColor();
-}
-
-function setBlue(value)
-{
-    if (check('blue', value)) {
-        parent.blue=value*1;
-    }
-    showColor();
-}
-
-function setColor(message)
-{
-    if (parent.over) {
-        var myColor = prompt(message, '#000000');
-        if (check('hex', myColor)) {
-            calcRGB(myColor);
-            fixColor();
-        }
-    }
-}
-
-function check(typ, wert)
-{
-    switch (typ)
-    {
-        case 'hue':
-            if (isNaN(wert) || wert<0 || wert>240) { return false; } else { return true; }
-        break;
-        case 'sat':
-            if (isNaN(wert) || wert<0 || wert>255) { return false; } else { return true; }
-        break;
-        case 'light':
-            if (isNaN(wert) || wert<-128 || wert>128) { return false; } else { return true; }
-        break;
-        case 'red':
-            if (isNaN(wert) || wert<0 || wert>255) { return false; } else { return true; }
-        break;
-        case 'green':
-            if (isNaN(wert) || wert<0 || wert>255) { return false; } else { return true; }
-        break;
-        case 'blue':
-            if (isNaN(wert) || wert<0 || wert>255) { return false; } else { return true; }
-        break;
-        case 'hex':
-            if (!wert || isNaN(parseInt("0x"+wert.substring(1,3))) || isNaN(parseInt("0x"+wert.substring(3,5))) || isNaN(parseInt("0x"+wert.substring(5,7)))) { return false; } else { return true; }
-        break;
-        default:
-            return true;
-        break;
-    }
-
-    return true;
-}
+var hexstring  = '#%REDHEX%%GREENHEX%%BLUEHEX%';
 
 function moveColor(event)
 {
@@ -156,10 +61,10 @@ function pickColor(event)
         hue = parseInt(event.layerX)-parseInt(event.target.x);
         sat = parseInt(event.layerY)-parseInt(event.target.y);
     }
-    
+
     hue = (hue<0) ? 0 : hue; hue = (hue>240) ? 240 : hue;
     sat = (sat<0) ? 0 : sat; sat = (sat>255) ? 255 : sat;
-    hue_alt = hue; sat_alt = sat; light_alt = light;
+    hue_old = hue; sat_old = sat; light_old = light;
 
     calcColor();
     fixColor();
@@ -172,9 +77,9 @@ function pickSw(event)
     } else {
         light = -event.layerY+event.target.y+128;
     }
-    
+
     light = (light<-128) ? -128 : light; light = (light>128) ? 128 : light;
-    hue_alt = hue; sat_alt = sat; light_alt = light;
+    hue_old = hue; sat_old = sat; light_old = light;
 
     calcColor();
     fixColor();
@@ -244,16 +149,6 @@ function calcColor()
 
 }
 
-function calcRGB(hexFarbe)
-{
-    redHex   = hexFarbe.substring(1,3);
-    greenHex = hexFarbe.substring(3,5);
-    blueHex  = hexFarbe.substring(5,7);
-    red   = parseInt("0x"+redHex);
-    green = parseInt("0x"+greenHex);
-    blue  = parseInt("0x"+blueHex);
-}
-
 function calcHex()
 {
     redHex   = red.toString(16);
@@ -266,20 +161,13 @@ function calcHex()
 
 function showColor()
 {
-    document.getElementById('hue').value = hue;
-    document.getElementById('sat').value = sat;
-    document.getElementById('light').value = light;
-    document.getElementById('red').value = red;
-    document.getElementById('green').value = green;
-    document.getElementById('blue').value = blue;
-
     calcHex();
 
     var disp = document.getElementById('color');
     disp.style.backgroundColor = "#"+redHex+greenHex+blueHex;
 
     disp = document.getElementById('hex_new');
-    txt = hexadez;
+    txt = hexstring;
     txt = txt.replace(/%REDHEX%/i, redHex);
     txt = txt.replace(/%GREENHEX%/i, greenHex);
     txt = txt.replace(/%BLUEHEX%/i, blueHex);
@@ -296,7 +184,7 @@ function fixColor()
     preview.style.backgroundColor = "#"+redHex+greenHex+blueHex;
 
     preview = document.getElementById('hex_old');
-    var txt = hexadez;
+    var txt = hexstring;
     txt = txt.replace(/%REDHEX%/i, redHex);
     txt = txt.replace(/%GREENHEX%/i, greenHex);
     txt = txt.replace(/%BLUEHEX%/i, blueHex);
@@ -306,7 +194,7 @@ function fixColor()
 
 function resetColor()
 {
-    hue = hue_alt; sat = sat_alt; light = light_alt;
+    hue = hue_old; sat = sat_old; light = light_old;
     calcColor();
     showColor();
 }
@@ -316,7 +204,6 @@ function resetColor()
  */
 function showColorPicker(e, elemName)
 {
-    var currentColor = document.getElementById(elemName).value;
     var colorSubmit = document.getElementById('color_submit');
     if (colorSubmit) {
         colorSubmit.onclick = function () {
@@ -349,10 +236,7 @@ function showColorPicker(e, elemName)
     } else {
         o.style.display = 'none';
     }
-    if (check('hex', currentColor)) {
-        calcRGB(currentColor);
-        fixColor();
-    }
+    fixColor();
 }
 /**
  * @author Mathias Weitz
