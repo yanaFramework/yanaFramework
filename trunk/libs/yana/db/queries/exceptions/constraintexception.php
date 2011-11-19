8d37418c3ce6ca4ce84410f6753b2c17fcfd60d9
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2011-11-19 13:47:22 +0000

    fixed namespace

--- trunk/libs/yana/db/queries/exceptions/constraintexception.php
+++ trunk/libs/yana/db/queries/exceptions/constraintexception.php
@@ -25,7 +25,7 @@
  * @license  http://www.gnu.org/licenses/gpl.txt
  */
 
-namespace Yana\Db\Queries;
+namespace Yana\Db\Queries\Exceptions;
 
 /**
  * <<exception>> When a query detects that a statement does not comply with an existing constraint.
@@ -38,7 +38,7 @@ namespace Yana\Db\Queries;
  * @package     yana
  * @subpackage  db
  */
-class ConstraintException extends \Yana\Db\Queries\QueryException
+class ConstraintException extends \Yana\Db\Queries\Exceptions\QueryException
 {
 
     // intentionally left blank
--- trunk/libs/yana/db/queries/exceptions/queryexception.php
+++ trunk/libs/yana/db/queries/exceptions/queryexception.php
@@ -25,7 +25,7 @@
  * @license  http://www.gnu.org/licenses/gpl.txt
  */
 
-namespace Yana\Db\Queries;
+namespace Yana\Db\Queries\Exceptions;
 
 /**
  * <<exception>> Base class for query related exceptions.
