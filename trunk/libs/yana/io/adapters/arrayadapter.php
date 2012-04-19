7c662ab7322e0e201d7b20774f9d2937c573a412
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2012-04-19 04:34:16 +0000

    new very basic class for mocking and use as parent class

---
+++ trunk/libs/yana/io/adapters/arrayadapter.php
@@ -0,0 +1,72 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Io\Adapters;
+
+/**
+ * <<adapter>> data adapter
+ *
+ * Null adapter, that basically does nothing at all.
+ * Use this for UnitTests!
+ *
+ * @access      public
+ * @package     yana
+ * @subpackage  core
+ */
+class ArrayAdapter extends \Yana\Core\AbstractCountableArray implements \Yana\Io\Adapters\IsDataAdapter
+{
+
+    /**
+     * Adds the item if it is missing.
+     *
+     * Same as:
+     * <code>
+     * $array[] = $subject;
+     * </code>
+     *
+     * @param  \SplSubject  $subject  what you want to add
+     */
+    public function update(\SplSubject $subject)
+    {
+        if (!\in_array($subject, $this->_getItems())) {
+            $this[] = $subject;
+        }
+    }
+
+    /**
+     * Return array of ids in use.
+     *
+     * @return  array
+     */
+    public function getIds()
+    {
+        return \array_keys($this->_getItems());
+    }
+
+}
+
+?>
