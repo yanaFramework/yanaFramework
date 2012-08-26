d62e23eeeb07b75860197b31444dcbf425eccf50
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2012-08-26 00:32:53 +0000

    added some basic mailer execptions

---
+++ trunk/libs/yana/core/exceptions/mails/mailexception.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Core\Exceptions\Mails;
+
+/**
+ * <<exception>> Base class for mail related exceptions.
+ *
+ * @package     yana
+ * @subpackage  core
+ */
+class MailException extends \Yana\Core\Exceptions\AbstractException
+{
+
+    /**
+     * Set id of duplicate entry.
+     *
+     * @param   scalar  $id  identifier of duplicate entry
+     * @return  \Yana\Core\Exceptions\AlreadyExistsException
+     */
+    public function setId($id)
+    {
+        $this->data = array('ID' => $id);
+        return $this;
+    }
+
+}
+
+?>
