e2cb8bd0391830bf2b252b1b6f5e3ec9795895d0
yanaFramework_cp <SND\yanaFramework_cp>
2012-10-17 20:12:37 +0000

    new data-provider collection added

---
+++ trunk/libs/yana/core/metadata/dataprovidercollection.php
@@ -0,0 +1,61 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Core\MetaData;
+
+/**
+ * Collection of data providers.
+ *
+ * @package     yana
+ * @subpackage  core
+ * @ignore
+ */
+class DataProviderCollection extends \Yana\Core\AbstractCollection
+{
+
+    /**
+     * Add or replace a data provider.
+     *
+     * @param   scalar                               $offset  index of item to replace
+     * @param   \Yana\Core\MetaData\IsDataProvider   $value   new value of item
+     * @throws  \Yana\Core\Exceptions\InvalidArgumentException  if the value is not a valid collection item
+     * @return  \Yana\Core\MetaData\IsDataProvider
+     */
+    public function offsetSet($offset, $value)
+    {
+        if (!$value instanceof \Yana\Core\MetaData\IsDataProvider) {
+            $message = 'Instance of IsDataProvider expected';
+            $level = \Yana\Log\TypeEnumeration::WARNING;
+            throw new \Yana\Core\Exceptions\InvalidArgumentException($message, $level);
+        }
+        $this->_offsetSet($offset, $value);
+        return $value;
+    }
+
+}
+
+?>
---
+++ trunk/libs/yana/core/metadata/nulldataprovider.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ *
+ * @ignore
+ */
+
+namespace Yana\Core\MetaData;
+
+/**
+ * <<template method>> 
+ *
+ * @package     yana
+ * @subpackage  core
+ */
+class NullDataProvider extends \Yana\Core\Object implements \Yana\Core\MetaData\IsDataProvider
+{
+
+    /**
+     * Load meta data object.
+     *
+     * @param   string  $id  identifier for the file to be loaded
+     * @return  \Yana\Core\MetaData\IsPackageMetaData
+     * @throws  \Yana\Core\Exceptions\NotFoundException  when the file for this identifier is not found
+     */
+    public function loadOject($id)
+    {
+        return new \Yana\Core\MetaData\PackageMetaData();
+    }
+
+}
+
+?>
