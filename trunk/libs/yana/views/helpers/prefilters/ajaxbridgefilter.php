0c9ca4bed5e1952c0e4154dcb011456f60c29aea
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2014-02-23 22:18:28 +0000

    refactoring (WIP) language-manager is no longer a singleton

--- trunk/libs/yana/views/helpers/prefilters/ajaxbridgefilter.php
+++ trunk/libs/yana/views/helpers/prefilters/ajaxbridgefilter.php
@@ -59,7 +59,7 @@ class AjaxBridgeFilter extends \Yana\Views\Helpers\AbstractViewHelper implements
                 '        window.yanaProfileId="' . \Yana::getId() . '";' . "\n" .
                 '        window.yanaSessionName="{$SESSION_NAME}";' . "\n" .
                 '        window.yanaSessionId="{$SESSION_ID}";' . "\n" .
-                '        window.yanaLanguage="' . \Yana::getInstance()->getLanguage()->getLanguage() . '";' . "\n" .
+                '        window.yanaLanguage="' . \Yana::getInstance()->getLanguage()->getLocale() . '";' . "\n" .
                 '        var src="";' . "\n" .
                 '        var php_self="' . $templateClass->getTemplateVars('PHP_SELF') . '";' . "\n" .
                 '        //--></script>';
