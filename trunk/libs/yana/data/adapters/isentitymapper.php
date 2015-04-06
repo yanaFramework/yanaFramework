bf8f5bb03aa216224b3d6a4ef0a5376f430f2cd2
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2015-04-06 12:52:58 +0000

    added isEntityMapper interface

---
+++ trunk/libs/yana/data/adapters/isentitymapper.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ *
+ * @ignore
+ */
+
+namespace Yana\Security\Users;
+
+/**
+ * <<interface>> For mapping entity objects to arrays and vice-versa.
+ *
+ * The mapper is usually bound to a certain database table, or file structure.
+ * It is part of a very simple approach to object-relational mapping.
+ *
+ * @package     yana
+ * @subpackage  io
+ */
+interface IsEntityMapper
+{
+
+    /**
+     * Creates an entity based on a database row.
+     *
+     * @param   array  $databaseRow  row containing user info
+     * @return  \Yana\Data\Adapters\IsEntity
+     * @throws  \Yana\Core\Exceptions\InvalidArgumentException  when the given data is invalid
+     */
+    public function toEntity(array $databaseRow);
+
+    /**
+     * Creates a database row based on an entity.
+     *
+     * @param   \Yana\Data\Adapters\IsEntity  $entity  entity containing the information you wish to map
+     * @return  array
+     */
+    public function toDatabaseRow(\Yana\Data\Adapters\IsEntity $entity);
+
+}
+
+?>
