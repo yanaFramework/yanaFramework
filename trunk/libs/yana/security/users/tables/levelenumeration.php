3f8781e566c7f252dc09c85cec49a550cf94c3f2
yanaFramework_cp <SND\yanaFramework_cp>
2015-04-07 22:21:30 +0000

    added table enumerations

--- trunk/libs/yana/security/users/tables/levelenumeration.php
+++ trunk/libs/yana/security/users/tables/levelenumeration.php
@@ -27,37 +27,22 @@
  * @ignore
  */
 
-namespace Yana\Security\Users;
+namespace Yana\Security\Users\Tables;
 
 /**
- * <<enumeration>> Column names for table user.
+ * <<enumeration>> Column names for table securitylevel.
  *
  * @package     yana
  * @subpackage  security
  *
  * @ignore
  */
-class UserColumnEnumeration extends \Yana\Core\AbstractEnumeration
+class LevelEnumeration extends \Yana\Core\AbstractEnumeration
 {
 
-    const TABLE = 'user';
-    const ID = 'USER_ID';
-    const PASSWORD = 'USER_PWD';
-    const SESSION_CHECKSUM = 'USER_SESSION';
-    const MAIL = 'USER_MAIL';
-    const IS_ACTIVE = 'USER_ACTIVE';
-    const LANGUAGE = 'USER_LANGUAGE';
-    const LOGIN_FAILURE_COUNT = 'USER_FAILURE_COUNT';
-    const LOGIN_FAILURE_TIME = 'USER_FAILURE_TIME';
-    const LOGIN_COUNT = 'USER_LOGIN_COUNT';
-    const LOGIN_TIME = 'USER_LOGIN_LAST';
-    const TIME_CREATED = 'USER_INSERTED';
-    const IS_EXPERT_MODE = 'USER_IS_EXPERT';
-    const RECENT_PASSWORDS = 'USER_PWD_LIST';
-    const PASSWORD_TIME = 'USER_PWD_TIME';
-    const PASSWORD_RECOVERY_ID = 'USER_RECOVER_ID';
-    const PASSWORD_RECOVERY_TIME = 'USER_RECOVER_UTC';
-    
+    const TABLE = 'securitylevel';
+    const USER = 'USER_ID';
+    const GRANTED_BY_USER = 'USER_CREATED';
 
 }
 
