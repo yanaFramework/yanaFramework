0d0c50252eb374520c07f4b5557bcbf2926b36b5
yanaFramework_cp <SND\yanaFramework_cp>
2015-05-09 15:23:20 +0000

    new Builder class for instaces of IsUser

---
+++ trunk/libs/yana/security/users/userbuilder.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ *
+ * @ignore
+ */
+
+namespace Yana\Security\Users;
+
+/**
+ * User manager.
+ *
+ * This persistent class provides access to user data and function to set logins and passwords.
+ *
+ * @package     yana
+ * @subpackage  security
+ *
+ * @ignore
+ */
+class UserBuilder extends \Yana\Core\Object
+{
+
+    /**
+     * @var  \Yana\Data\Adapters\IsDataAdapter
+     */
+    private $_userAdapter = null;
+
+    /**
+     * <<constructor>> Set up and initialize database connection.
+     *
+     * @param  \Yana\Data\Adapters\IsDataAdapter  $userAdapter  inject a NULL-adapter for Unit-tests
+     */
+    public function __construct(\Yana\Data\Adapters\IsDataAdapter $userAdapter = null)
+    {
+        if (!isset($userAdapter)) {
+            $userAdapter = new \Yana\Security\Users\UserAdapter(\Yana\Application::connect('user'));
+        }
+        $this->_userAdapter = $userAdapter;
+    }
+    /**
+     * @return  \Yana\Data\Adapters\IsDataAdapter
+     */
+    protected function _getUserAdapter()
+    {
+        return $this->_userAdapter;
+    }
+
+    /**
+     * Build an user object from the current user name saved in the session data.
+     *
+     * @param   array  $sessionData  with the user name at index 'user_name'
+     * @return  \Yana\Security\Users\GuestUser
+     */
+    public function buildFromSession(array $sessionData = null)
+    {
+        if (\is_null($sessionData)) {
+            $sessionData = $_SESSION;
+        }
+        if (!isset($sessionData['user_name'])) {
+            return new \Yana\Security\Users\GuestUser();
+        }
+
+        assert('!isset($userId); // Cannot redeclare var $userId');
+        $userId = $sessionData['user_name'];
+        assert('!isset($adapter); // Cannot redeclare var $adapter');
+        $adapter = $this->_getUserAdapter();
+
+        if (!isset($adapter[$userId])) {
+            return new \Yana\Security\Users\GuestUser();
+        }
+        assert('!isset($userAccount); // Cannot redeclare var $userAccount');
+        $userAccount = $adapter[$userId];
+
+        assert('$userAccount instanceof \Yana\Security\Users\IsUser; // Return value must be an instance of IsUser');
+        return $userAccount;
+    }
+
+}
+
+?>
