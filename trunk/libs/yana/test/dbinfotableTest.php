c7c9d505a904434c90e1075d6848dbe6381c47dc
yanaFramework_cp <SND\yanaFramework_cp>
2016-04-01 23:02:16 +0000

    updated test-cases

--- trunk/libs/yana/test/dbinfotableTest.php
+++ trunk/libs/yana/test/dbinfotableTest.php
@@ -39,8 +39,7 @@ class DbInfoTableTest extends PHPUnit_Framework_TestCase
 {
 
     /**
-     * @var    Plugins\DbTools\InfoTable
-     * @access protected
+     * @var  \Plugins\DbTools\InfoTable
      */
     protected $dbinfotable;
 
@@ -58,8 +57,6 @@ class DbInfoTableTest extends PHPUnit_Framework_TestCase
     /**
      * Sets up the fixture, for example, opens a network connection.
      * This method is called before a test is executed.
-     *
-     * @access protected
      */
     protected function setUp()
     {
@@ -73,8 +70,6 @@ class DbInfoTableTest extends PHPUnit_Framework_TestCase
     /**
      * Tears down the fixture, for example, closes a network connection.
      * This method is called after a test is executed.
-     *
-     * @access protected
      */
     protected function tearDown()
     {
