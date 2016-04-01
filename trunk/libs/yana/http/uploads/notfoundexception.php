72b6f6be140f483a72179685b102a06b5c757594
yanaFramework_cp <yanaFramework@users.noreply.github.com>
2016-04-01 22:14:36 +0000

    added http namespace

---
+++ trunk/libs/yana/http/uploads/notfoundexception.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * YANA library
+ *
+ * Primary controller class
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Http\Uploads;
+
+/**
+ * <<exception>> When entry was not found in $_FILES array.
+ *
+ * @package     yana
+ * @subpackage  http
+ */
+class NotFoundException extends \Yana\Http\Uploads\UploadException
+{
+
+    // Intenionally left blank
+
+}
+
+?>
