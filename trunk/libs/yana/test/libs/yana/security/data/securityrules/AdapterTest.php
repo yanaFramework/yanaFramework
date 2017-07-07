ec282765f2136912e7483de8f92cba7197918150
YanaFramework@users.noreply.github.com <YanaFramework@users.noreply.github.com>
2017-07-08 01:58:57 +0200

    refactoring of security sub-system cont'd

---
+++ trunk/libs/yana/test/libs/yana/security/data/securityrules/AdapterTest.php
@@ -0,0 +1,104 @@
+<?php
+/**
+ * PHPUnit test-case
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  test
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Security\Data\SecurityRules;
+
+/**
+ * @ignore
+ */
+require_once __DIR__ . '/../../../../../include.php';
+
+/**
+ * Test-case
+ *
+ * @package  test
+ */
+class AdapterTest extends \PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @var \Yana\Db\NullConnection
+     */
+    protected $connection;
+
+    /**
+     * @var Adapter
+     */
+    protected $object;
+
+    /**
+     * Constructor
+     *
+     * @ignore
+     */
+    public function __construct()
+    {
+        \Yana\Db\Ddl\DDL::setDirectory(CWD. 'resources/');
+    }
+
+    /**
+     * Sets up the fixture, for example, opens a network connection.
+     * This method is called before a test is executed.
+     */
+    protected function setUp()
+    {
+        try {
+
+            \Yana\Db\FileDb\Driver::setBaseDirectory(CWD. 'resources/db/');
+            \Yana\Db\Ddl\DDL::setDirectory(CWD. 'resources/');
+            $schema = \Yana\Files\XDDL::getDatabase('user');
+            $this->connection = new \Yana\Db\FileDb\Connection($schema);
+            restore_error_handler();
+
+        } catch (\Exception $e) {
+            $this->markTestSkipped("Unable to connect to database");
+        }
+        $this->object = new \Yana\Security\Data\SecurityRules\Adapter($this->connection);
+    }
+
+    /**
+     * Tears down the fixture, for example, closes a network connection.
+     * This method is called after a test is executed.
+     */
+    protected function tearDown()
+    {
+        
+    }
+
+    /**
+     * @test
+     */
+    public function testFindEntities()
+    {
+        $entities = $this->object->findEntities('administrator', 'default');
+        $this->assertCount(3, $entities);
+        $this->assertEquals((new \Yana\Security\Data\SecurityRules\Rule('ADMIN', 'DEFAULT', true)), $entities[0]);
+        $this->assertEquals((new \Yana\Security\Data\SecurityRules\Rule('', 'PRINT', true)), $entities[1]);
+        $this->assertEquals((new \Yana\Security\Data\SecurityRules\Rule('ADMIN', 'ADMIN', true)), $entities[2]);
+    }
+
+}
