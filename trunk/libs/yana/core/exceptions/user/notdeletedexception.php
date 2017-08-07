7962866a12f9c55fde4f1626e4a207c6086b4fe5
YanaFramework <yanaframework@users.noreply.github.com>
2017-08-07 22:26:01 +0200

    just some new exceptions and the obligatory tests to go with them

---
+++ trunk/libs/yana/core/exceptions/user/notdeletedexception.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Core\Exceptions\User;
+
+/**
+ * <<exception>> User management issue.
+ *
+ * Thrown when a data-adapter is unable to delete a security setting.
+ *
+ * @package     yana
+ * @subpackage  core
+ */
+class NotDeletedException extends \Yana\Core\Exceptions\User\UserException
+{
+
+    /**
+     * Create a new instance.
+     *
+     * @param  string      $message   the message that should be reported
+     * @param  int         $code      optional error code
+     * @param  \Exception  $previous  use this when you need to rethrow a catched exception
+     */
+    public function __construct($message = "", $code = \E_USER_ERROR, \Exception $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+    }
+
+}
+
+?>
