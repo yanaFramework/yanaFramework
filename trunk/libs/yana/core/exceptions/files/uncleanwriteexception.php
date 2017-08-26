ee1cd29eac7c302e9368d003ecf8b8ef1ab6b729
YanaFramework@users.noreply.github.com <YanaFramework@users.noreply.github.com>
2017-08-26 10:21:01 +0200

    added UncleanWriteException

---
+++ trunk/libs/yana/core/exceptions/files/uncleanwriteexception.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Core\Exceptions\Files;
+
+/**
+ * <<exception>> When the file source has changed while editing.
+ *
+ * When two users edit the same file and both submit conflicting changes,
+ * the second user to submit should get this exception.
+ *
+ * @package     yana
+ * @subpackage  core
+ */
+class UncleanWriteException extends \Yana\Core\Exceptions\Files\FileException
+{
+    /* intentionally left blank */
+}
+
+?>
