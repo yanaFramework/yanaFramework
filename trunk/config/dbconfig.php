c4fd859d3bd6fd09290aa7febf5e4c7c5a19839e
YanaFramework@users.noreply.github.com <YanaFramework@users.noreply.github.com>
2019-07-21 21:46:13 +0200

    Switched default driver to mysqli instead of mysql

--- trunk/config/dbconfig.php
+++ trunk/config/dbconfig.php
@@ -1,6 +1,6 @@
 <?php
         if (!defined('YANA_DATABASE_ACTIVE'))   define('YANA_DATABASE_ACTIVE', false);
-        if (!defined('YANA_DATABASE_DBMS'))     define('YANA_DATABASE_DBMS', "mysql");
+        if (!defined('YANA_DATABASE_DBMS'))     define('YANA_DATABASE_DBMS', "mysqli");
         if (!defined('YANA_DATABASE_HOST'))     define('YANA_DATABASE_HOST', "localhost");
         if (!defined('YANA_DATABASE_PORT'))     define('YANA_DATABASE_PORT', "");
         if (!defined('YANA_DATABASE_USER'))     define('YANA_DATABASE_USER', "root");
