56841f243e2c54ebf8f1d00cf8034f1fb875667c
YanaFramework@users.noreply.github.com <YanaFramework@users.noreply.github.com>
2019-09-15 21:34:38 +0200

    improved documentation of return type

--- trunk/libs/yana/db/ddl/factories/isdoctrinewrapper.php
+++ trunk/libs/yana/db/ddl/factories/isdoctrinewrapper.php
@@ -24,6 +24,7 @@
  * @package  yana
  * @license  http://www.gnu.org/licenses/gpl.txt
  */
+declare(strict_types=1);
 
 namespace Yana\Db\Ddl\Factories;
 
@@ -64,7 +65,7 @@ interface IsDoctrineWrapper
      * Get list of columns associated with the table.
      *
      * @param   string  $tableName  must be a valid database table
-     * @return  array
+     * @return  \Doctrine\DBAL\Schema\Column[]
      */
     public function listTableColumns($tableName);
 
