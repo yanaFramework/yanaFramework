ed3c44c9853d51395754321af44e3c09c63377f9
YanaFramework@users.noreply.github.com <YanaFramework@users.noreply.github.com>
2019-09-15 21:30:23 +0200

    refactoring to make table names case sensitive (as required by a small number of RDBMS, for example when table names are quoted)

--- trunk/libs/yana/db/ddl/constraint.php
+++ trunk/libs/yana/db/ddl/constraint.php
@@ -24,6 +24,7 @@
  * @package  yana
  * @license  http://www.gnu.org/licenses/gpl.txt
  */
+declare(strict_types=1);
 
 namespace Yana\Db\Ddl;
 
@@ -37,7 +38,7 @@ namespace Yana\Db\Ddl;
  * @package     yana
  * @subpackage  db
  */
-class Constraint extends \Yana\Db\Ddl\AbstractObject
+class Constraint extends \Yana\Db\Ddl\AbstractUnnamedObject
 {
     /**#@+
      * @ignore
