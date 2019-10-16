d3d4fe6a15650efa6be719484d63c4acbfe53dc4
YanaFramework <yanaframework@users.noreply.github.com>
2019-10-17 00:10:13 +0200

    Introduced some new exceptions

---
+++ trunk/libs/yana/core/exceptions/files/alreadyexistsexception.php
@@ -0,0 +1,43 @@
+<?php
+/**
+ * YANA library
+ *
+ * Software:  Yana PHP-Framework
+ * Version:   {VERSION} - {DATE}
+ * License:   GNU GPL  http://www.gnu.org/licenses/
+ *
+ * This program: can be redistributed and/or modified under the
+ * terms of the GNU General Public License as published by the
+ * Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see http://www.gnu.org/licenses/.
+ *
+ * This notice MAY NOT be removed.
+ *
+ * @package  yana
+ * @license  http://www.gnu.org/licenses/gpl.txt
+ */
+
+namespace Yana\Core\Exceptions\Files;
+
+/**
+ * <<exception>> File or directory already exists.
+ *
+ * Thrown when trying to create a file or directory, but another of the same name does already exist.
+ *
+ * @package     yana
+ * @subpackage  core
+ */
+class AlreadyExistsException extends \Yana\Core\Exceptions\Files\FileException
+{
+    /* intentionally left blank */
+}
+
+?>
